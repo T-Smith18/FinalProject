ff7115b5f78c4aa71ce1ae87020698ea8ca45e16
Mark Smith <marks273@gmail.com>
2018-04-29 09:47:59 -0700

    add Game class/add animation/update framework

---
+++ constants.py
@@ -0,0 +1,13 @@
+# Global constants
+ 
+# Colors
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+GREEN = (0, 255, 0)
+RED = (255, 52, 52)
+BLUE = (52, 52, 255)
+ 
+# Screen dimensions
+SCREEN_WIDTH = 1200
+SCREEN_HEIGHT = 600
+FPS = 60
